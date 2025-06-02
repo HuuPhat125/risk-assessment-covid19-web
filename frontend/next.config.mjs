ca83e6f2813c323742d236c7d7c249915bd84e2a
HuuPhat125 <phatd142@gmail.com>
2025-06-02 17:33:15 +0700

    fix: fix config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // <--- dòng này là bắt buộc để next export hoạt động
-};
+const nextConfig = {};
 
 export default nextConfig;
+
